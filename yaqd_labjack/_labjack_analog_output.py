3a0dbfe026676065f9e0dc5380364dc2710cc32f
Blaise Thompson <blaise@untzag.com>
2023-04-21 14:51:16 -0500

    Analog output (#37)
    * analog output
    
    * avpr

---
+++ yaqd_labjack/_labjack_analog_output.py
@@ -0,0 +1,39 @@
+__all__ = ["LabjackAnalogOutput"]
+
+import asyncio
+from typing import Dict, Any, List
+
+from pymodbus.client import ModbusTcpClient  # type: ignore
+from yaqd_core import IsDiscrete, HasPosition, IsDaemon
+
+from ._bytes import *
+
+
+class LabjackAnalogOutput(IsDiscrete, HasPosition, IsDaemon):
+    _kind = "labjack-analog-output"
+
+    clients: Dict[str, ModbusTcpClient] = {}
+
+    def __init__(self, name, config, config_filepath):
+        super().__init__(name, config, config_filepath)
+        #
+        if self._config["address"] in LabjackAnalogOutput.clients:
+            self._client = LabjackDigitalOutput.clients[self._config["address"]]
+        else:
+            self._client = ModbusTcpClient(config["address"])
+            LabjackDigitalOutput.clients[self._config["address"]] = self._client
+
+    def _set_position(self, value: float) -> None:
+        self._client.write_registers(self._config["modbus_address"], float32_to_data(value))
+
+    async def update_state(self):
+        while True:
+            if self._busy:
+                await asyncio.sleep(0.1)
+                value = self._state["destination"]
+                self._state["position"] = value
+                self._busy = False
+            try:
+                await asyncio.wait_for(self._busy_sig.wait(), timeout=1)
+            except asyncio.TimeoutError:
+                self._set_position(self._state["destination"])
